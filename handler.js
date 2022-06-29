cdf72a8ec2b7bcf56675cf1dde2ab90235a5a2c7
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2022-06-29 18:42:00 +0530

    CPT30

--- handler.js
+++ handler.js
@@ -1,8 +1,31 @@
 const aws = require("aws-sdk");
+const dynamoDb = new aws.DynamoDB.DocumentClient();
 
 aws.config.loadFromPath("./skillreactor/config.json");
 
+
+
 module.exports.handle = async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: 'CryptoPortfolioTracker-user-sahith05',
+    Item: {
+      username: data.username,
+      email: data.email,
+      password: data.password,
+    }
+  }
+
+  let lo = dynamoDb.put(params, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    const response = {
+      statusCode: 200,
+    };
+  })
+
+  console.log(lo);
   return {
     statusCode: 200,
     headers: {
@@ -12,4 +35,8 @@ module.exports.handle = async (event, context) => {
     },
     body: "Hello!",
   };
+
+
+
+
 };
