2fa2fc6aa1e935bb0f2b09d19073f8afee2257bd
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2022-06-29 21:23:38 +0530

    CPT30

--- handler.js
+++ handler.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const aws = require("aws-sdk");
 const dynamoDb = new aws.DynamoDB.DocumentClient();
 
@@ -6,14 +7,16 @@ aws.config.loadFromPath("./skillreactor/config.json");
 
 
 module.exports.handle = async (event, context) => {
-  let data = JSON.stringify(event.body);
-
+  const data = JSON.stringify(event.body);
+  const val = data.split(",")[2].split(":")[1].split("\"")[1].replace("\\", '');
+  const hash = crypto.createHash('sha256').update(val).digest('hex').toString();
+  // console.log("hash:",hash);
   const params = {
     TableName: 'CryptoPortfolioTracker-user-sahith05',
     Item: {
       username: data.username,
       email: data.email,
-      password: data.password,
+      password:hash,
     }
   }
 
