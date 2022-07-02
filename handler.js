42d6349558dd315fdd04f2541a75e9ce850d348c
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2022-07-02 12:46:40 +0530

    CPT30

--- handler.js
+++ handler.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 const aws = require("aws-sdk");
 const dynamoDb = new aws.DynamoDB.DocumentClient();
-
 aws.config.loadFromPath("./skillreactor/config.json");
 
 // console.log(data);
@@ -10,61 +9,105 @@ aws.config.loadFromPath("./skillreactor/config.json");
 
 // console.log("hash:",hash);
 
+
 module.exports.handle = async (event, context) => {
-  // const data = JSON.parse(event.body);
+  let data = event.body;
+  console.log(data);
 
-  if (event.body.username && event.body.password && event.body.email) {
-    const data = JSON.stringify(event.body);
-    let user = data.split(",")[0].split(":")[1].split("\"")[1].replace("\\", '');
-    console.log(user);
-    let mail = data.split(",")[1].split(":")[1].split("\"")[1].replace("\\", '');
-    console.log(mail);
-    let pass = data.split(",")[2].split(":")[1].split("\"")[1].replace("\\", '');
-    console.log(pass);
-    let hash = crypto.createHash('sha256').update(pass).digest('hex');
-    console.log(hash);
-    const params = {
-      TableName: 'CryptoPortfolioTracker-user-sahith05',
-      Item: {
-        username: user,
-        email: mail,
-        password: hash,
-      }
+  let pass = JSON.parse(data);
+  console.log(pass.password);
+  let hash = crypto.createHash('sha256').update(pass.password).digest('hex');
+  console.log("hash:",hash);
+
+  let params = {
+    TableName: 'CryptoPortfolioTracker-user-sahith05',
+    Item: {
+      username: pass.username,
+      email: pass.email,
+      password: hash
     }
+  }
 
-    try {
-      await dynamoDb.put(params).promise();
-      return {
-        statusCode: 200,
-        headers: {
-          "Content-Type": "*/*",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "*",
-        },
-        body: "sucess"
-      }
-    } catch (err) {
-      console.log(err);
-      return {
-        statusCode: 400,
-        headers: {
-          "Content-Type": "*/*",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "*",
-        },
-        body: err
-      }
+  try {
+    let result =  await dynamoDb.put(params).promise();
+    return {
+      statusCode: 200,
+      headers: {
+                "Content-Type": "*/*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*",
+              },
+      body: JSON.stringify(result)
     }
-
-  } else {
+  }catch(err){
+    console.log(err);
     return {
       statusCode: 400,
       headers: {
-        "Content-Type": "*/*",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-      },
-    body: "Please provide all the details"
+                "Content-Type": "*/*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "*",
+              },
+      body: JSON.stringify(err)
     }
   }
+
+
+
 }
+
+// module.exports.handle = async (event, context) => {
+  
+//   const data = JSON.stringify(event.body);
+//   let user = data.split(",")[0].split(":")[1].split("\"")[1].replace("\\", '');
+//   // console.log(user);
+//   let mail = data.split(",")[1].split(":")[1].split("\"")[1].replace("\\", '');
+//   // console.log(mail);
+//   let pass = data.split(",")[2].split(":")[1].split("\"")[1].replace("\\", '');
+//   // console.log(pass);
+//   let hash = crypto.createHash('sha256').update(pass).digest('hex');
+//   // console.log(hash);
+//   const params = {
+//     TableName: 'CryptoPortfolioTracker-user-sahith05',
+//     Item: {
+//       username: user,
+//       email: mail,
+//       password: hash,
+//     }
+//   }
+
+//   try {
+//     await dynamoDb.put(params).promise();
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Content-Type": "*/*",
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Methods": "*",
+//       },
+//       body: "sucess"
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     return {
+//       statusCode: 400,
+//       headers: {
+//         "Content-Type": "*/*",
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Methods": "*",
+//       },
+//       body: err
+//     }
+//   }
+  // } else {
+  //   return {
+  //     statusCode: 400,
+  //     headers: {
+  //       "Content-Type": "*/*",
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Access-Control-Allow-Methods": "*",
+  //     },
+  //     body: "Please provide all the details"
+  //   }
+  // }
+// }
