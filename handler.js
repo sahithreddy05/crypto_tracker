058c6b40cb4a17713f117904c0d3cdad136fb02b
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2022-07-20 12:51:58 +0530

    CPT100

--- handler.js
+++ handler.js
@@ -1,15 +1,82 @@
 const aws = require("aws-sdk");
 
 aws.config.loadFromPath("./skillreactor/config.json");
+let client = new aws.DynamoDB.DocumentClient();
+
 
 module.exports.handle = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "*/*",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "*",
-    },
-    body: "Hello!",
-  };
+
+  let data = JSON.parse(event.body);
+  // let token = data.token;
+  if (data.username && data.token && data.quantity) {
+    var params = {
+      TableName: "CryptoPortfolioTracker-user-sahith05",
+      Key: { username: data.username },
+      UpdateExpression: "set assets = :t ",
+      ExpressionAttributeValues: {
+        ":t": {
+          "bitcoin": {
+            "quantity": data.quantity
+          },
+          "ethereum": {
+            "quantity": data.quantity
+          }
+        }
+      },
+      ReturnValues: "UPDATED_NEW"
+    }
+
+    await client.update(params, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(data);
+      }
+    }).promise();
+
+    // data => {
+    //   // console.log(data);
+    //    client.update(params1, (err, data) => {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+    //     else {
+    //       console.log(data);
+    //     }
+    //   });
+    // }
+    // var params1 = {
+    //   TableName: "CryptoPortfolioTracker-user-sahith05",
+    //   Key: { username: data.username },
+    //   UpdateExpression: `set assets = :t `,
+    //   ExpressionAttributeValues: {
+    //     ":t": { 'quantity': data.quantity }
+    //   },
+    //   ReturnValues: "UPDATED_NEW"
+    // }
+
+    // ['quantity'] = data.quantity
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "*/*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: "Hello!",
+    };
+  }
+  else {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "*/*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: "Hello!",
+    };
+  }
 };
