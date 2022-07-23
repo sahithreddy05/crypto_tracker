35a9ef896cfd73df8e177df0b4e286a45d2badfa
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2022-07-23 10:56:57 +0530

    CPT120

--- handler.js
+++ handler.js
@@ -1,15 +1,51 @@
 const aws = require("aws-sdk");
 
 aws.config.loadFromPath("./skillreactor/config.json");
+var dynamo = require('dynamodb');
+dynamo.AWS.config.loadFromPath("./skillreactor/config.json");
+
+var tableName = dynamo.define('tableName', {
+  hashKey: 'username',
+  tableName: "CryptoPortfolioTracker-user-sahith05"
+});
 
 module.exports.handle = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "*/*",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "*",
-    },
-    body: "Hello!",
-  };
+
+  // console.log(event.queryStringParameters);
+
+  var username = event.queryStringParameters.username;
+  console.log(username);
+  var res = await tableName.get(username);
+  if (username) {
+    //   res = await tableName.get(username, function (err, acc) {
+    //     if (err) {
+    //       console.log(err);
+    //     }
+    //     else {
+    //       return (acc.attrs.assets);
+    //     }
+    //   });
+
+    console.log(res);
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "*/*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: JSON.stringify(res.attrs.assets)
+    };
+  }
+  else {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "*/*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
+      body: JSON.stringify("Username not provided")
+    };
+  }
 };
