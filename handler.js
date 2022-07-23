617b103cb9367af7759276e9ed6341232fda037e
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2022-07-23 11:09:03 +0530

    CPT120

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ aws.config.loadFromPath("./skillreactor/config.json");
 var dynamo = require('dynamodb');
 dynamo.AWS.config.loadFromPath("./skillreactor/config.json");
 
-var tableName = dynamo.define('tableName', {
+var tableName = dynamo.define('CryptoPortfolioTracker-user-sahith05', {
   hashKey: 'username',
   tableName: "CryptoPortfolioTracker-user-sahith05"
 });
