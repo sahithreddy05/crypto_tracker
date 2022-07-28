80d2bbe0b067bc4b4d78f254f0fc7da5893429af
sahithreddy05 <45757789+sahithreddy05@users.noreply.github.com>
2022-07-28 21:38:51 +0530

    CPT140

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const aws = require("aws-sdk");
+const axios = require("axios");
 
 aws.config.loadFromPath("./skillreactor/config.json");
 var dynamo = require('dynamodb');
@@ -9,20 +10,24 @@ var tableName = dynamo.define('CryptoPortfolioTracker-user-sahith05', {
   tableName: "CryptoPortfolioTracker-user-sahith05"
 });
 
-module.exports.handle = async (event, context) => {
 
-  // console.log(event.queryStringParameters);
+module.exports.handle = async (event, context,callback) => {
+
   try {
     var username = event.queryStringParameters.username;
     console.log(username);
     var res = await tableName.get(username);
-    // console.log(res.attrs.assets);
-    var i = Object.keys(res.attrs.assets);
+    var key = Object.keys(res.attrs.assets);
     var ans = [];
-    for (var j = 0; j < i.length; j++) {
-      ans.push({ token: i[j], quantity: res.attrs.assets[i[j]].quantity });
-      // console.log(Object.values(res.attrs.assets)[j]);
+    var price;
+    for (var j = 0; j < key.length; j++) {
+      let log = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${key[j]}&vs_currencies=inr`)
+      console.log(log.data[key[j]].inr);
+      price = log.data[key[j]].inr;
+      totalValue = res.attrs.assets[key[j]].quantity * price;
+      ans.push({ token: key[j], quantity: res.attrs.assets[key[j]].quantity, price: price, allocation: 33, totalValue: totalValue });
     }
+
     return {
       statusCode: 200,
       headers: {
@@ -33,6 +38,7 @@ module.exports.handle = async (event, context) => {
       body: JSON.stringify(ans)
     };
   } catch (err) {
+    console.log(err);
     return {
       statusCode: 400,
       headers: {
